8c970a6b7a570f0248b33906ca45fd5ac53de2d9
Gm Awesome <gmmoose17@gmail.com>
2022-01-02 14:53:11 +0800

    ✨ updated ✨

--- birthday-reminder/src/components/Home.js
+++ birthday-reminder/src/components/Home.js
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="container">
+    <div className="container" style={{ textAlign: "center" }}>
       <h3>Cousin Birthday</h3>
       <Link to="/couzdetails">Cousin Details</Link>
+      <br />
+      <Link to="/couzbirthday">Today Birthday</Link>
     </div>
   );
 };
