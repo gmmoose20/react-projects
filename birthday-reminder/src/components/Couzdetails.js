3194a8726cb43b3c842069cebc8abcf6004651f7
Gm Awesome <gmmoose17@gmail.com>
2022-01-05 14:56:28 +0800

    ✨ updated ✨

--- birthday-reminder/src/components/Couzdetails.js
+++ birthday-reminder/src/components/Couzdetails.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Couzdetails = () => {
   return (
-    <div>
-      <h1>Cousin Details</h1>
+    <div className="container">
+      <h3>Cousin Birthday Details</h3>
+      <Link to="/couzbirthday">Today Birthday</Link>
     </div>
   );
 };
