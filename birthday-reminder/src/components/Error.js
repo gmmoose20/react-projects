6262ea303af768249d98d9a4bbc2e8f645539d13
Gm Awesome <gmmoose17@gmail.com>
2022-01-05 12:32:37 +0800

    ✨ updated ✨

--- birthday-reminder/src/components/Error.js
+++ birthday-reminder/src/components/Error.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Error = () => {
   return (
-    <div>
-      <h1>Error Page</h1>
+    <div className="error_page">
+      <h1>!Oops Page not found</h1>
     </div>
   );
 };
