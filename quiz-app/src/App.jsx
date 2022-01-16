c7993d959a56d7121fc49398ab9e8f0cc0e1bce4
Gm Awesome <gmmoose17@gmail.com>
2022-01-16 17:02:43 +0800

    ✨ updated

--- quiz-app/src/App.jsx
+++ quiz-app/src/App.jsx
@@ -4,7 +4,26 @@ function App() {
   return (
     <div className="app">
       <div className="main">main</div>
-      <div className="pyramid">pyramid</div>
+      <div className="pyramid">
+        <ul className="pointsList">
+          <li className="pointsListItem">
+            <span className="pointsListItemRound">4</span>
+            <span className="pointsListItemAmount">400</span>
+          </li>
+          <li className="pointsListItem">
+            <span className="pointsListItemRound">3</span>
+            <span className="pointsListItemAmount">300</span>
+          </li>
+          <li className="pointsListItem">
+            <span className="pointsListItemRound">2</span>
+            <span className="pointsListItemAmount">200</span>
+          </li>
+          <li className="pointsListItem">
+            <span className="pointsListItemRound">1</span>
+            <span className="pointsListItemAmount">100</span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
